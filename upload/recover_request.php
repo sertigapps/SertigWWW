a465a42005e0c17ed34540ea1fc084608c3ad7a1
Luis Garcia <lrglm16@gmail.com>
2018-07-26 14:26:12 -0500

    damn semicolom

--- upload/recover_request.php
+++ upload/recover_request.php
@@ -44,7 +44,7 @@ if($data->emailaddress!=''){
             $headers = "MIME-Version: 1.0" . "\r\n";
             $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
             $headers .= "X-Priority: 3\r\n";
-            $headers .= "X-Mailer: PHP". phpversion() ."\r\n" 
+            $headers .= "X-Mailer: PHP". phpversion() ."\r\n"; 
 
             // More headers
             $headers .= "Organization: Sertig Apps\r\n";
