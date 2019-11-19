37b501cef636ba1f360aa8b7a5b7af73a9f487c0
Renato Garcia <informacion@sertigapps.com>
2019-11-19 13:15:16 -0600

    Update accio_labels.php

--- upload/accio_labels.php
+++ upload/accio_labels.php
@@ -16,6 +16,7 @@
 
 
 include('lumos_image_validation.php'); // getExtension Method
+header("Access-Control-Allow-Origin: *");
 $valid = true;
 $angularJSData = json_decode(file_get_contents("php://input"));
 $angularJSData = (array)$angularJSData;
